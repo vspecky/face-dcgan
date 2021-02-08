4e533842da8efd0da9a1b54fc1c4e9d5cf8569bd
vspecky <thelonenerd9913@gmail.com>
2021-02-08 21:26:27 +0530

    Add data preparation scripts

---
+++ prepare_color.py
@@ -0,0 +1,16 @@
+import os
+import cv2 as cv
+from tqdm import tqdm
+
+this_path = os.path.dirname(__file__)
+img_path = os.path.join(this_path, "faces", "faces")
+
+imgs = os.listdir(img_path)
+
+tqdm_imgs = tqdm(imgs, unit="Images")
+
+for img_p in tqdm_imgs:
+    path = os.path.join(img_path, img_p)
+    big_img = cv.imread(path)
+    small_img = cv.resize(big_img, (64, 64))
+    cv.imwrite(path, small_img)
